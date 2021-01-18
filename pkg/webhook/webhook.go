cc64fe08520db58137dfe712789c2cc732beb41a
Andrew Block <andy.block@gmail.com>
2021-01-17 23:35:05 -0600

    Management of non Docker or S2I build types

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -63,7 +63,7 @@ func (wsvr *WebhookServer) Handle(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 
-		// Get QuayIngegration
+		// Get QuayIntegration
 		quayIntegration, found, err := wsvr.getQuayIntegration(&ar)
 
 		if !found {
@@ -124,7 +124,7 @@ func getAdmissionResponseForBuild(ar *v1beta1.AdmissionReview, quayIntegration *
 
 	quayRegistryHostname, err := quayIntegration.GetRegistryHostname()
 
-	if build.Spec.CommonSpec.Output.To.Kind != "ImageStreamTag" {
+	if buildv1.JenkinsPipelineBuildStrategyType == build.Spec.Strategy.Type || build.Spec.CommonSpec.Output.To == nil || build.Spec.CommonSpec.Output.To.Kind != "ImageStreamTag" {
 		return &v1beta1.AdmissionResponse{
 			Allowed: true,
 		}
